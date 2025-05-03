fdd6d5b118e59ad00e499ebbc88f91eb1dabbc87
IDHRES AHMED MI <48350502+idreescg@users.noreply.github.com>
2025-05-03 16:31:38 +0530

    Update loki.sh

--- loki.sh
+++ loki.sh
@@ -24,7 +24,15 @@ helm upgrade --install $RELEASE_NAME $CHART_NAME \
   --set fluent-bit.enabled=true \
   --set promtail.enabled=true \
   --set loki.config.limits_config.volume_enabled=true \
-  --set loki.config.limits_config.allow_structured_metadata=true
-
+  --set loki.config.limits_config.allow_structured_metadata=true \
+  --set loki.storage.type=s3 \
+  --set loki.storage.s3.bucketnames=loki-logs-ahmed \
+  --set loki.storage.s3.endpoint=s3.amazonaws.com \
+  --set loki.schemaConfig.configs[0].from=2024-01-01 \
+  --set loki.schemaConfig.configs[0].store=boltdb-shipper \
+  --set loki.schemaConfig.configs[0].object_store=s3 \
+  --set loki.schemaConfig.configs[0].schema=v12 \
+  --set loki.schemaConfig.configs[0].index.prefix=index_ \
+  --set loki.schemaConfig.configs[0].index.period=24h
 
 echo "Loki stack setup completed."
