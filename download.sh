dc1098dd4629be0226c38dab44098600aa02fce8
IDHRES AHMED MI <48350502+idreescg@users.noreply.github.com>
2025-05-13 15:21:36 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,16 +1,18 @@
-wget  https://raw.githubusercontent.com/idreescg/my-eks/refs/heads/main/PrometheusAutomation.sh
+#!/bin/bash
 
+# Download the required files
+wget https://raw.githubusercontent.com/idreescg/my-eks/refs/heads/main/PrometheusAutomation.sh
 wget https://raw.githubusercontent.com/idreescg/my-eks/refs/heads/main/aws-controller.sh
-
 wget https://raw.githubusercontent.com/idreescg/my-eks/refs/heads/main/loki-values.yaml
-
 wget https://raw.githubusercontent.com/idreescg/my-eks/refs/heads/main/promtail-values.yaml
 
+# Make scripts executable
+chmod +x aws-controller.sh PrometheusAutomation.sh
 
-run the below command 
-
+# Run the aws-controller script
 sh aws-controller.sh
-
+# Wait for 30 seconds
 sleep 30
 
+# Run the Prometheus automation script
 sh PrometheusAutomation.sh
